a7785b1ccf96cf6a86f119cbb0d2014228766b0b
John Daugherty <johndaugherty1986@gmail.com>
2017-10-17 23:43:12 -0500

    youtube view

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyB5ybOmLuIJXwW0MKprAbugzxoWu5EKJlE'
 
+YTSearch({key: API_KEY, term: "surfboards"}, function(data){
+  console.log(data);
+});
+
 const App = () => {
-  return <div>hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
