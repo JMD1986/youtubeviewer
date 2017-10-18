b9b7eaa1da62c640007051348143d9b412a2d697
John Daugherty <johndaugherty1986@gmail.com>
2017-10-18 00:53:03 -0500

    making render a class function, adding search functionality

--- src/index.js
+++ src/index.js
@@ -5,16 +5,26 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyB5ybOmLuIJXwW0MKprAbugzxoWu5EKJlE'
 
-YTSearch({key: API_KEY, term: "surfboards"}, function(data){
-  console.log(data);
-});
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: "surfboards"}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render (){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
