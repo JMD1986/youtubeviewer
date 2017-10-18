96ab99bf3288757ae2c1e83e784bfc0fb6980d5d
John Daugherty <johndaugherty1986@gmail.com>
2017-10-18 12:52:11 -0500

    what went wrong?

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyB5ybOmLuIJXwW0MKprAbugzxoWu5EKJlE'
 
@@ -9,7 +10,7 @@ const API_KEY = 'AIzaSyB5ybOmLuIJXwW0MKprAbugzxoWu5EKJlE'
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = { videos: [] };
 
@@ -18,10 +19,11 @@ class App extends Component {
     });
   }
 
-  render (){
+  render(){
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
