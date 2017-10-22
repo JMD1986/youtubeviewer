eae699f2e2ed8b7570a751913c9fa863cb14e0e0
John Daugherty <johndaugherty1986@gmail.com>
2017-10-21 21:49:59 -0500

    embedded video in index

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -9,6 +9,8 @@ const VideoListItem = ({video}) => {
           <div className="media-left">
             <img className="media-object" src={imageUrl} />
           </div>
+
+
           <div className="media-body">
             <div className="media-heading">{video.snippet.title}
             </div>
